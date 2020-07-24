3056efde59b9cbbf83cbd0ac1a3fc2e8e78e2d6e
sp <jaas0407@gmail.com>
2020-07-24 23:06:29 +0800

    created network interface

---
+++ terraform/projects/sing-app/env/dev/network_interface.tf
@@ -0,0 +1,10 @@
+resource "aws_network_interface" "main" {
+  subnet_id       =  "${element(["${data.aws_subnet.private_subnet_1.id}", 
+  "${data.aws_subnet.private_subnet_2.id}"], count.index)}"
+}
+
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  count                = 2
+  security_group_id    = "${aws_security_group.private_sg.id}"
+  network_interface_id = "${element(aws_instance.linux_instance.*.primary_network_interface_id, count.index)}"
+}
