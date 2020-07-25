f7f6b1826a13738f88a6b1c43952923e9cc30ccd
sp <jaas0407@gmail.com>
2020-07-25 14:01:39 +0800

    Testing network interface

--- terraform/projects/sing-app/env/dev/network_interface.tf
+++ terraform/projects/sing-app/env/dev/network_interface.tf
@@ -1,13 +1,19 @@
-# resource "aws_network_interface" "main" {
-#   count           = 2
-#   subnet_id       =  "${element(["${data.aws_subnet.private_subnet_1.id}", 
-#   "${data.aws_subnet.private_subnet_2.id}"], count.index)}"
+resource "aws_network_interface" "main" {
+  count           = 2
+  subnet_id       =  "${element(["${data.aws_subnet.private_subnet_1.id}", 
+  "${data.aws_subnet.private_subnet_2.id}"], count.index)}"
 
-#    depends_on = ["aws_security_group.instance_security_group"]
-# }
+  attachment {
+    instance     = "${element(aws_instance.linux_instance.*.id, count.index)}"
+    device_index = 1
+  }
 
-resource "aws_network_interface_sg_attachment" "sg_attachment" {
-  count                = 2
-  security_group_id    = "${aws_security_group.private_sg.id}"
-  network_interface_id = "${element(aws_instance.linux_instance.*.primary_network_interface_id, count.index)}"
+
+   depends_on = ["aws_security_group.instance_security_group"]
 }
+
+# resource "aws_network_interface_sg_attachment" "sg_attachment" {
+#   count                = 2
+#   security_group_id    = "${aws_security_group.private_sg.id}"
+#   network_interface_id = "${element(aws_instance.linux_instance.*.primary_network_interface_id, count.index)}"
+# }
