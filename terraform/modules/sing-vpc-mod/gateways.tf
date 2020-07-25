c723cd23579b5534fc189a2bff7a04a9f83c8e3a
sp <jaas0407@gmail.com>
2020-07-25 15:04:13 +0800

    updated vpc and ec2 details

--- terraform/modules/sing-vpc-mod/gateways.tf
+++ terraform/modules/sing-vpc-mod/gateways.tf
@@ -30,16 +30,30 @@ resource "aws_nat_gateway" "ngw_main" {
 # Route tables
 #*************
 
-resource "aws_route_table" "public-route" {
-    vpc_id = "${aws_vpc.main.id}"
+resource "aws_default_route_table" "public-route" {
+  default_route_table_id = "${aws_vpc.main.default_route_table_id}"
+
     route {
         cidr_block = "${var.default_route}"
         gateway_id = "${aws_internet_gateway.igw_main.id}"
     }
+
     tags = {
         Name = "${var.project}-public-route"
     }
 }
+
+# you can use if you need sepearte route table
+# resource "aws_route_table" "public-route" {
+#     vpc_id = "${aws_vpc.main.id}"
+#     route {
+#         cidr_block = "${var.default_route}"
+#         gateway_id = "${aws_internet_gateway.igw_main.id}"
+#     }
+#     tags = {
+#         Name = "${var.project}-public-route"
+#     }
+# }
 
 resource "aws_route_table" "private-route" {
     vpc_id = "${aws_vpc.main.id}"
@@ -74,8 +88,14 @@ resource "aws_route_table_association" "private-route-assoc2" {
 
 resource "aws_route_table_association" "public-route-assoc" {
     subnet_id = "${aws_subnet.public_subnet_1.id}"
-    route_table_id = "${aws_route_table.public-route.id}"
+    route_table_id = "${aws_default_route_table.public-route.id}"
 }
+
+# you can use if you want sepearte route table
+# resource "aws_route_table_association" "public-route-assoc" {
+#     subnet_id = "${aws_subnet.public_subnet_1.id}"
+#     route_table_id = "${aws_route_table.public-route.id}"
+# }
 
 #***********************************
 
--- terraform/modules/sing-vpc-mod/naclRules.tf
+++ terraform/modules/sing-vpc-mod/naclRules.tf
@@ -1,28 +1,55 @@
-resource "aws_network_acl" "main_nacl" {
-  vpc_id = "${aws_vpc.main.id}"
+# To create new Network ACL's"
+# resource "aws_network_acl" "main_nacl" {
+#   vpc_id = "${aws_vpc.main.id}"
+
+#   ingress {
+#     protocol   = "tcp"
+#     rule_no    = 100
+#     action     = "allow"
+#     cidr_block = "${var.default_route}"
+#     from_port  = 0
+#     to_port    = 0
+#   }
+
+#   egress {
+#     protocol   = "tcp"
+#     rule_no    = 100
+#     action     = "allow"
+#     cidr_block = "${var.default_route}"
+#     from_port  = 0
+#     to_port    = 0
+#   }
+#   tags = {
+#     Name = "${var.project}-nacl-rules"
+#   }
+# }
+#********* Variables **********
+
+#variable project {}
+#variable default_route {}
+
+# use the existing Network_ACL
+resource "aws_default_network_acl" "default" {
+  default_network_acl_id = aws_vpc.main.default_network_acl_id
 
   ingress {
-    protocol   = "tcp"
+    protocol   = -1
     rule_no    = 100
     action     = "allow"
-    cidr_block = "${var.default_route}"
+    cidr_block = aws_vpc.main.cidr_block
     from_port  = 0
     to_port    = 0
   }
 
   egress {
-    protocol   = "tcp"
+    protocol   = -1
     rule_no    = 100
     action     = "allow"
-    cidr_block = "${var.default_route}"
+    cidr_block = "0.0.0.0/0"
     from_port  = 0
     to_port    = 0
   }
-  tags = {
+    tags = {
     Name = "${var.project}-nacl-rules"
   }
 }
-#********* Variables **********
-
-#variable project {}
-#variable default_route {}
